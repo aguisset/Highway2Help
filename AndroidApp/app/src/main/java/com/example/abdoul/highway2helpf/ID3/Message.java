c6a2d84847e37e5cdd0bc1182500a20da3708818
aguisset <guissetabdoul@gmail.com>
2017-07-05 23:30:34 -0500

    Upload app

---
+++ AndroidApp/app/src/main/java/com/example/abdoul/highway2helpf/ID3/Message.java
@@ -0,0 +1,25 @@
+/**
+ * Created by Abdoul W. Guisset
+ */
+
+package com.example.abdoul.highway2helpf.ID3;
+
+public class Message {
+    private int code;
+    private String message;
+
+    public int getCode() {
+        return code;
+    }
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
+
